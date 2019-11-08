00145d60fe9357a078e45c30c9e5712cf608ae53
lookoutapiano <abdulmaula.nacan@gmail.com>
2019-11-08 19:58:10 +0800

    move

--- GeneSys/patient/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class PatientConfig(AppConfig):
-    name = 'patient'
--- GeneSys/patient/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Patient
-
-def patient(request):
-    context = {
-        'patients': Patient.objects.all(),
-    }
-    return render(request, 'patient/patient.html', context)
