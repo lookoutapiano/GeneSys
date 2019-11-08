b1e7f012456e69532c317aec33f5809c17e9a103
lookoutapiano <abdulmaula.nacan@gmail.com>
2019-11-08 20:05:59 +0800

    this is try

--- home/views.py
+++ home/views.py
@@ -6,15 +6,7 @@ def base(request):
     return render(request, 'home/base.html')
 
 def home(request):
-<<<<<<< HEAD
-    return render(request, 'home/home.html', {'title':'Test'})
-
-def about(request):
-    return render(request, 'home/about.html', {'title':'About'})
-
-=======
     return render(request, 'home/home.html')
 
 def about(request):
     return render(request, 'home/about.html', {'title':'About'})
->>>>>>> 03d6c7011696ecd24024fa82bac10eaac7daa114
