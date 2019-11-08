85e033ce59ba6180e8b74247590ebbdef8728e4d
lookoutapiano <abdulmaula.nacan@gmail.com>
2019-11-08 19:42:11 +0800

    test commit

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ def base(request):
     return render(request, 'home/base.html')
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {'title':'Test'})
 
 def about(request):
     return render(request, 'home/about.html', {'title':'About'})
